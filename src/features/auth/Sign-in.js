7145524ba1904a8ff76cd8d060671e2a68490753
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-11-18 16:44:49 +0600

    code cleanup, minor fix

--- src/features/auth/Sign-in.js
+++ src/features/auth/Sign-in.js
@@ -61,7 +61,7 @@ const Signin = () => {
                     value={password}
                     onChange={onPasswordChanged}
                     />
-                    <button onClick={onSubmit}>Login{<div className="lds-hourglass"></div>}</button>
+                    <button onClick={onSubmit}>Login{hourglass && <div className="lds-hourglass"></div>}</button>
                 </form>
         </div>
     )
